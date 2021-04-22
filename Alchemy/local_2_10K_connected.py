fe576e7756e7d018f5cd1bea61e8cdbe5171cf48
Christopher Morris <christopher.morris@udo.edu>
2021-04-22 13:23:36 -0400

    Started implementation.

--- Alchemy/local_2_10K_connected.py
+++ Alchemy/local_2_10K_connected.py
@@ -76,7 +76,7 @@ class Alchemy(InMemoryDataset):
             data.edge_index_1 = edge_index_1
             data.edge_index_2 = edge_index_2
 
-            one_hot = np.eye(83)[node_labels[i]]
+            one_hot = np.eye(49)[node_labels[i]]
             data.x = torch.from_numpy(one_hot).to(torch.float)
             data.y = data.y = torch.from_numpy(np.array([targets[i]])).to(torch.float)
 
