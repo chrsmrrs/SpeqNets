157d192acc4a7bc812520b796fe11ac1dad83584
Christopher Morris <christopher.morris@udo.edu>
2021-04-26 18:08:03 -0400

    MC.

--- ZINC/local_2_50K_connected_plus.py
+++ ZINC/local_2_50K_connected_plus.py
@@ -138,8 +138,8 @@ class NetGIN(torch.nn.Module):
 
         num_features = 242
 
-        self.nn_all = Sequential(Linear(652, dim), torch.nn.BatchNorm1d(dim), ReLU(), Linear(dim, dim),
-                           torch.nn.BatchNorm1d(dim), ReLU())
+        # self.nn_all = Sequential(Linear(652, dim), torch.nn.BatchNorm1d(dim), ReLU(), Linear(dim, dim),
+        #                    torch.nn.BatchNorm1d(dim), ReLU())
 
         nn1_1 = Sequential(Linear(num_features, dim), ReLU(), Linear(dim, dim))
         nn1_2 = Sequential(Linear(num_features, dim), ReLU(), Linear(dim, dim))
@@ -183,7 +183,7 @@ class NetGIN(torch.nn.Module):
         self.bn6 = torch.nn.BatchNorm1d(dim)
         self.mlp_6 = Sequential(Linear(2 * dim, dim), ReLU(), Linear(dim, dim))
 
-        self.fc1 = Linear(6 * dim + dim, dim)
+        self.fc1 = Linear(6 * dim + 652, dim)
         self.fc2 = Linear(dim, dim)
         self.fc3 = Linear(dim, dim)
         self.fc4 = Linear(dim, 1)
@@ -225,7 +225,7 @@ class NetGIN(torch.nn.Module):
 
         x = torch.cat([x_1_r, x_2_r, x_3_r, x_4_r, x_5_r, x_6_r], dim=-1)
         x = global_mean_pool(x, data.batch)
-        x_all = self.nn_all(x_all)
+        #x_all = self.nn_all(x_all)
         x_all = global_mean_pool(x_all, data.batch_all)
 
         x = torch.cat([x, x_all], dim=-1)
