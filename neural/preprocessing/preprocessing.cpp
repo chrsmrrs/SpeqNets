b5867a3cad75f513e7f6182dca149e8087f38f14
Christopher Morris <christopher.morris@udo.edu>
2021-10-30 21:33:28 -0400

    w

--- neural/preprocessing/preprocessing.cpp
+++ neural/preprocessing/preprocessing.cpp
@@ -274,12 +274,14 @@ tuple <Attributes, Attributes, Attributes> get_attributes_2_1(const Graph &g) {
     Attributes attributes;
     cout << "AA" << endl;
     attributes = g.get_attributes();
+    cout << attributes.size() << endl;
     cout << "BB" << endl;
 
     EdgeAttributes edge_attributes;
 
     cout << "CC" << endl;
     edge_attributes = g.get_edge_attributes();
+    cout << edge_attributes.size() << endl;
     cout << "DD" << endl;
 
     Attributes first;
