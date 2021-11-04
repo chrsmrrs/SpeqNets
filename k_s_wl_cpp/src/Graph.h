2f522b8a3f2f9bce392a232322dca9c9ec0be0ff
Christopher Morris <christopher.morris@udo.edu>
2021-11-04 19:31:54 -0400

    Preprocessing

--- k_s_wl_cpp/src/Graph.h
+++ k_s_wl_cpp/src/Graph.h
@@ -4,7 +4,8 @@
 #define WLFAST_GRAPH_H
 
 #ifdef __linux__
-#include <eigen3/Eigen/Sparse>
+#include "../eigen-3.4.0/Eigen/Sparse"
+//#include <eigen3/Eigen/Sparse>
 #else
 #include "../eigen-3.4.0/Eigen/Sparse"
 #endif
