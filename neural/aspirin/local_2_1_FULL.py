a8db18d98dcc2f4f359ae48197d04dd16a200047
Christopher Morris <christopher.morris@udo.edu>
2021-11-04 12:11:20 -0400

    w

--- neural/aspirin/local_2_1_FULL.py
+++ neural/aspirin/local_2_1_FULL.py
@@ -172,7 +172,7 @@ class NetGIN(torch.nn.Module):
 
         x = F.relu(self.fc1(x))
         x = self.fc4(x)
-        return x
+        return x.view(-1)
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
