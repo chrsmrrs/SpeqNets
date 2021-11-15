fb03fbceb9a60be6529daaa0284b9d9bc8a79a2f
Christopher Morris <christopher.morris@udo.edu>
2021-11-15 12:34:05 -0500

    Preprocessing

--- k_s_wl_cpp/implementation/tud_benchmark/main_kernel.py
+++ k_s_wl_cpp/implementation/tud_benchmark/main_kernel.py
@@ -7,7 +7,7 @@ from auxiliarymethods.kernel_evaluation import linear_svm_evaluation
 
 def main():
     ### Smaller datasets using LIBSVM.
-    dataset = [["IMDB-BINARY", False], ["ENZYMES", True], ["IMDB-MULTI", False], ["NCI1", True], ["PROTEINS", True],
+    dataset = [["ENZYMES", True], ["IMDB-BINARY", False], ["IMDB-MULTI", False], ["NCI1", True], ["PROTEINS", True],
                ["REDDIT-BINARY", False], ["PTC_MR", True],["MUTAG", True]]
 
     # Number of repetitions of 10-CV.
@@ -82,7 +82,44 @@ def main():
     results.append(dataset + " " + "WLP3_2 " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
 
+    # Number of repetitions of 10-CV.
+    num_reps = 3
 
+    ### Larger datasets using LIBLINEAR with edge labels.
+    dataset = [["MOLT-4", True, True], ["Yeast", True, True], ["MCF-7", True, True],
+               ["github_stargazers", False, False],
+               ["reddit_threads", False, False]]
+
+    for d, use_labels, use_edge_labels in dataset:
+        dataset = d
+        classes = dp.get_dataset(dataset)
+
+        # 1-WL kernel, number of iterations in [1:6].
+        all_matrices = []
+        for i in range(1, 6):
+            gm = kb.compute_wl_1_sparse(dataset, i, use_labels, use_edge_labels)
+            gm_n = aux.normalize_feature_vector(gm)
+            all_matrices.append(gm_n)
+
+        acc, s_1, s_2 = linear_svm_evaluation(all_matrices, classes, num_repetitions=num_reps, all_std=True)
+        print(d + " " + "WL1SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "WL1SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+        # 1-WL kernel, number of iterations in [1:6].
+        all_matrices = []
+        for i in range(1, 6):
+            gm = kb.compute_wl_2_1_sparse(dataset, i, use_labels, use_edge_labels)
+            gm_n = aux.normalize_feature_vector(gm)
+            all_matrices.append(gm_n)
+
+        acc, s_1, s_2 = linear_svm_evaluation(all_matrices, classes, num_repetitions=num_reps, all_std=True)
+        print(d + " " + "WL21SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "WL21SP " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+
+
+    for r in results:
+        print(r)
 
 
 
