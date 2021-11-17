037d970918734debd14ea31a1b71d3646fb3f92b
Christopher Morris <christopher.morris@udo.edu>
2021-11-17 11:15:07 -0500

    w

--- neural_node/ogb_arxiv.py
+++ neural_node/ogb_arxiv.py
@@ -48,7 +48,8 @@ class Arxiv(InMemoryDataset):
         data = dataset[0]
         data.adj_t = data.adj_t.to_symmetric()
 
-        print(data.adj_t)
+        print(data.adj_t2)
+        print(data.adj_t2[0])
         exit()
 
         x = data.x.cpu().detach().numpy()
