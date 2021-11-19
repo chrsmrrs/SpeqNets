5c7bec6863b0f343d28c8b02d4d5c2af5058d68a
Christopher Morris <christopher.morris@udo.edu>
2021-11-18 20:09:03 -0500

    `w`

--- k_s_wl_cpp/implementation/tud_benchmark/main_kernel.py
+++ k_s_wl_cpp/implementation/tud_benchmark/main_kernel.py
@@ -3,10 +3,10 @@ import kernel_baselines as kb
 import auxiliarymethods.datasets as dp
 from auxiliarymethods.kernel_evaluation import kernel_svm_evaluation
 from auxiliarymethods.kernel_evaluation import linear_svm_evaluation
-from svm.auxiliarymethods.auxiliary_methods import read_lib_svm, normalize_gram_matrix, normalize_feature_vector
 import os.path
 from os import path as pth
 import numpy as np
+
 def main():
     num_reps = 10
 
@@ -38,7 +38,7 @@ def main():
             gm_n = aux.normalize_gram_matrix(gm)
             all_matrices.append(gm_n)
         acc, s_1, s_2 = kernel_svm_evaluation(all_matrices, classes, num_repetitions=num_reps, all_std=True)
-        print(dataset + " " + "WL2 " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        print(dataset + " " + "q " + str(acc) + " " + str(s_1) + " " + str(s_2))
         results.append(dataset + " " + "WL2 " + str(acc) + " " + str(s_1) + " " + str(s_2))
 
         all_matrices = []
