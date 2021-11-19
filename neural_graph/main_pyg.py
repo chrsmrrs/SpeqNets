e753037f24a9afce699bf401bf701f0e753f9fd3
Christopher Morris <christopher.morris@udo.edu>
2021-11-18 19:29:53 -0500

    `w`

--- neural_graph/main_pyg.py
+++ neural_graph/main_pyg.py
@@ -133,7 +133,7 @@ def eval(model, device, loader, evaluator):
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    dataset = PygGraphPropPredDataset(name="ogbg-mollipo")
+    dataset = PygGraphPropPredDataset(name="ogbg-molclintox")
     print(len(dataset))
 
     feature = 'full'
@@ -147,7 +147,7 @@ def main():
 
     split_idx = dataset.get_idx_split()
 
-    evaluator = Evaluator("ogbg-mollipo")
+    evaluator = Evaluator("ogbg-molclintox")
 
     train_loader = DataLoader(dataset[split_idx["train"]], batch_size=32, shuffle=True,
                               num_workers=0)
@@ -198,3 +198,4 @@ if __name__ == "__main__":
 # ogbg-moltoxcast: 0.6189788458360118
 # ogbg-molbbbp: 0.6867283950617283
 # ogbg-mollipo: 0.770088
+# ogbg-molclintox
