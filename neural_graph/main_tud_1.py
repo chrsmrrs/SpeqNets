294de3384bf13a4cedc3dffa35b6fc2e9af2cc53
Christopher Morris <christopher.morris@udo.edu>
2021-11-20 16:08:57 -0500

    `w`

--- neural_graph/main_tud_1.py
+++ neural_graph/main_tud_1.py
@@ -94,7 +94,7 @@ for _ in range(5):
 
     train_dataset = dataset[0:4000]
     val_dataset = dataset[4000:4500]
-    test_dataset = dataset[5000:]
+    test_dataset = dataset[4500:]
 
     batch_size = 25
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
