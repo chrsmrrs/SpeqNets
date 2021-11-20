b5c70f17f6650972b553ee8a6c0b94bcfb434175
Christopher Morris <christopher.morris@udo.edu>
2021-11-20 17:33:13 -0500

    `w`

--- neural_graph/main_tud_1.py
+++ neural_graph/main_tud_1.py
@@ -14,6 +14,19 @@ import torch.nn.functional as F
 
 import numpy as np
 
+import os.path as osp
+import numpy as np
+from torch.nn import Linear as Lin
+from torch.nn import Sequential, Linear, ReLU
+from torch_geometric.nn import Set2Set
+
+from torch_geometric.data import (InMemoryDataset, Data)
+from torch_geometric.data import DataLoader
+
+import torch
+from torch_geometric.nn import MessagePassing
+import torch.nn.functional as F
+
 class GINConv(MessagePassing):
     def __init__(self, emb_dim, dim1, dim2):
         super(GINConv, self).__init__(aggr="add")
@@ -35,53 +48,62 @@ class GINConv(MessagePassing):
         return aggr_out
 
 
-class NetGIN(torch.nn.Module):
+class GINConv(MessagePassing):
+    def __init__(self, emb_dim, dim1, dim2):
+        super(GINConv, self).__init__(aggr="add")
+
+        self.bond_encoder = Sequential(Linear(emb_dim, dim1), ReLU(), Linear(dim1, dim1))
+        self.mlp = Sequential(Linear(dim1, dim1), ReLU(), Linear(dim1, dim2))
+
+        self.eps = torch.nn.Parameter(torch.Tensor([0]))
+
+    def forward(self, x, edge_index, edge_attr):
+        edge_embedding = self.bond_encoder(edge_attr)
+
+        out = self.mlp((1 + self.eps) * x + self.propagate(edge_index, x=x, edge_attr=edge_embedding))
+
+        return out
+
+    def message(self, x_j, edge_attr):
+        return F.relu(x_j + edge_attr)
+
+    def update(self, aggr_out):
+        return aggr_out
+
+
+class NetGINE(torch.nn.Module):
     def __init__(self, dim):
-        super(NetGIN, self).__init__()
+        super(NetGINE, self).__init__()
 
         num_features = 6
+        dim = dim
 
-        self.conv1_1 = GINConv(4, num_features, dim)
-        self.bn1 = torch.nn.BatchNorm1d(dim)
+        self.conv1 = GINConv(4, num_features, dim)
+        self.conv2 = GINConv(4, dim, dim)
+        self.conv3 = GINConv(4, dim, dim)
+        self.conv4 = GINConv(4, dim, dim)
+        self.conv5 = GINConv(4, dim, dim)
+        self.conv6 = GINConv(4, dim, dim)
 
-        self.conv2_1 = GINConv(4, dim, dim)
-        self.bn2 = torch.nn.BatchNorm1d(dim)
+        self.set2set = Set2Set(1 * dim, processing_steps=6)
 
-        self.conv3_1 = GINConv(4, dim, dim)
-        self.bn3 = torch.nn.BatchNorm1d(dim)
-
-        self.conv4_1 = GINConv(4, dim, dim)
-        self.bn4 = torch.nn.BatchNorm1d(dim)
-
-        self.fc1 = Linear(4 * dim, dim)
-        self.fc2 = Linear(dim, dim)
-        self.fc3 = Linear(dim, dim)
-        self.fc4 = Linear(dim, 1)
+        self.fc1 = Lin(2 * dim, dim)
+        self.fc4 = Linear(dim, 12)
 
     def forward(self, data):
         x = data.x
-        edge_attr = data.edge_attr
 
-        x_1 = F.relu(self.conv1_1(x, data.edge_index, edge_attr))
-        x_1_r = self.bn1(x_1)
-
-        x_2 = F.relu(self.conv2_1(x_1_r, data.edge_index, edge_attr))
-        x_2_r = self.bn2(x_2)
-
-        x_3 = F.relu(self.conv3_1(x_2_r, data.edge_index, edge_attr))
-        x_3_r = self.bn3(x_3)
-
-        x_4 = F.relu(self.conv4_1(x_3_r, data.edge_index, edge_attr))
-        x_4_r = self.bn4(x_4)
-
-        x = torch.cat([x_1_r, x_2_r, x_3_r, x_4_r], dim=-1)
-        x = global_mean_pool(x, data.batch)
-
+        x_1 = F.relu(self.conv1(x, data.edge_index, data.edge_attr))
+        x_2 = F.relu(self.conv2(x_1, data.edge_index, data.edge_attr))
+        x_3 = F.relu(self.conv3(x_2, data.edge_index, data.edge_attr))
+        x_4 = F.relu(self.conv4(x_3, data.edge_index, data.edge_attr))
+        x_5 = F.relu(self.conv5(x_4, data.edge_index, data.edge_attr))
+        x_6 = F.relu(self.conv6(x_5, data.edge_index, data.edge_attr))
+        x = x_6
+        x = self.set2set(x, data.batch)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
         x = self.fc4(x)
-        return x.view(-1)
+        return x
 
 
 plot_all = []
@@ -91,6 +113,11 @@ for _ in range(5):
     plot_it = []
     path = osp.join(osp.dirname(osp.realpath(__file__)), '.', 'datasets', "alchemy_full")
     dataset = TUDataset(path, name="alchemy_full")[0:20000].shuffle()
+
+    mean = dataset.data.y.mean(dim=0, keepdim=True)
+    std = dataset.data.y.std(dim=0, keepdim=True)
+    dataset.data.y = (dataset.data.y - mean) / std
+    mean, std = mean.to(device), std.to(device)
 
     train_dataset = dataset[0:18000]
     val_dataset = dataset[18000:19000]
@@ -102,63 +129,66 @@ for _ in range(5):
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = NetGIN(256).to(device)
+    model = NetGINE(256).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
                                                            factor=0.5, patience=10,
                                                            min_lr=0.0000001)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = NetGINE(64).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
+                                                           factor=0.5, patience=5,
+                                                           min_lr=0.0000001)
+
 
     def train():
         model.train()
         loss_all = 0
 
         lf = torch.nn.L1Loss()
-
         for data in train_loader:
             data = data.to(device)
             optimizer.zero_grad()
             loss = lf(model(data), data.y)
+
             loss.backward()
             loss_all += loss.item() * data.num_graphs
             optimizer.step()
-        return loss_all / len(train_loader.dataset)
+        return (loss_all / len(train_loader.dataset))
 
 
+    @torch.no_grad()
     def test(loader):
         model.eval()
-        error = 0
+        error = torch.zeros([1, 12]).to(device)
 
         for data in loader:
             data = data.to(device)
-            error += (model(data) - data.y).abs().sum().item()  # MAE
-        return error / len(loader.dataset)
+            error += ((data.y * std - model(data) * std).abs() / std).sum(dim=0)
+
+        error = error / len(loader.dataset)
+        error_log = torch.log(error)
+
+        return error.mean().item(), error_log.mean().item()
 
 
     best_val_error = None
-    test_error = None
-    for epoch in range(1, 1001):
+    for epoch in range(1, 201):
         lr = scheduler.optimizer.param_groups[0]['lr']
         loss = train()
-        val_error = test(val_loader)
-        scheduler.step(val_error)
+        val_error, _ = test(val_loader)
 
-        if best_val_error is None or val_error < best_val_error:
-            test_error = test(test_loader)
+        scheduler.step(val_error)
+        if best_val_error is None or val_error <= best_val_error:
+            test_error, test_error_log = test(test_loader)
             best_val_error = val_error
 
-        plot_it.append([loss, val_error, test_error])
         print('Epoch: {:03d}, LR: {:.7f}, Loss: {:.7f}, Validation MAE: {:.7f}, '
-              'Test MAE: {:.7f}'.format(epoch, lr, loss, val_error, test_error))
+              'Test MAE: {:.7f},Test MAE: {:.7f}, '.format(epoch, lr, loss, val_error, test_error, test_error_log))
 
         if lr < 0.000001:
             print("Converged.")
-            plot_all.append(plot_it)
             break
 
 
-    results.append(test_error)
-
-print("########################")
-print(results)
-results = np.array(results)
-print(results.mean(), results.std())
