8b4efae48bd02fdd002cf0df6855ebf87ba93020
Christopher Morris <christopher.morris@udo.edu>
2021-11-22 18:16:56 -0500

    `w`

--- neural_graph/gcn_1.py
+++ neural_graph/gcn_1.py
@@ -1,57 +1,77 @@
 import os.path as osp
 
 import torch
+from torch.nn import Linear
 import torch.nn.functional as F
-from torch_geometric.datasets import PPI
-from torch_geometric.loader import DataLoader
-from torch_geometric.nn import GATConv
 from sklearn.metrics import f1_score
+from torch_geometric.datasets import PPI
+import torch_geometric.transforms as T
+from torch_geometric.nn import GCN2Conv
+from torch_geometric.loader import DataLoader
 
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'PPI')
-train_dataset = PPI(path, split='train')
-val_dataset = PPI(path, split='val')
-test_dataset = PPI(path, split='test')
-train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'GCN2_PPI')
+pre_transform = T.Compose([T.GCNNorm(), T.ToSparseTensor()])
+train_dataset = PPI(path, split='train', pre_transform=pre_transform)
+val_dataset = PPI(path, split='val', pre_transform=pre_transform)
+test_dataset = PPI(path, split='test', pre_transform=pre_transform)
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
 
 
 class Net(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_channels, num_layers, alpha, theta,
+                 shared_weights=True, dropout=0.0):
         super(Net, self).__init__()
-        self.conv1 = GATConv(train_dataset.num_features, 256, heads=4)
-        self.lin1 = torch.nn.Linear(train_dataset.num_features, 4 * 256)
-        self.conv2 = GATConv(4 * 256, 256, heads=4)
-        self.lin2 = torch.nn.Linear(4 * 256, 4 * 256)
-        self.conv3 = GATConv(4 * 256, train_dataset.num_classes, heads=6,
-                             concat=False)
-        self.lin3 = torch.nn.Linear(4 * 256, train_dataset.num_classes)
 
-    def forward(self, x, edge_index):
-        x = F.elu(self.conv1(x, edge_index) + self.lin1(x))
-        x = F.elu(self.conv2(x, edge_index) + self.lin2(x))
-        x = self.conv3(x, edge_index) + self.lin3(x)
+        self.lins = torch.nn.ModuleList()
+        self.lins.append(Linear(train_dataset.num_features, hidden_channels))
+        self.lins.append(Linear(hidden_channels, train_dataset.num_classes))
+
+        self.convs = torch.nn.ModuleList()
+        for layer in range(num_layers):
+            self.convs.append(
+                GCN2Conv(hidden_channels, alpha, theta, layer + 1,
+                         shared_weights, normalize=False))
+
+        self.dropout = dropout
+
+    def forward(self, x, adj_t):
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = x_0 = self.lins[0](x).relu()
+
+        for conv in self.convs:
+            h = F.dropout(x, self.dropout, training=self.training)
+            h = conv(h, x_0, adj_t)
+            x = h + x
+            x = x.relu()
+
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.lins[1](x)
+
         return x
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Net().to(device)
-loss_op = torch.nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+model = Net(hidden_channels=2048, num_layers=9, alpha=0.5, theta=1.0,
+            shared_weights=False, dropout=0.2).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+criterion = torch.nn.BCEWithLogitsLoss()
 
 
 def train():
     model.train()
 
-    total_loss = 0
+    total_loss = total_examples = 0
     for data in train_loader:
         data = data.to(device)
         optimizer.zero_grad()
-        loss = loss_op(model(data.x, data.edge_index), data.y)
-        total_loss += loss.item() * data.num_graphs
+        loss = criterion(model(data.x, data.adj_t), data.y)
         loss.backward()
         optimizer.step()
-    return total_loss / len(train_loader.dataset)
+        total_loss += loss.item() * data.num_nodes
+        total_examples += data.num_nodes
+    return total_loss / total_examples
 
 
 @torch.no_grad()
@@ -61,14 +81,14 @@ def test(loader):
     ys, preds = [], []
     for data in loader:
         ys.append(data.y)
-        out = model(data.x.to(device), data.edge_index.to(device))
+        out = model(data.x.to(device), data.adj_t.to(device))
         preds.append((out > 0).float().cpu())
 
     y, pred = torch.cat(ys, dim=0).numpy(), torch.cat(preds, dim=0).numpy()
     return f1_score(y, pred, average='micro') if pred.sum() > 0 else 0
 
 
-for epoch in range(1, 101):
+for epoch in range(1, 2001):
     loss = train()
     val_f1 = test(val_loader)
     test_f1 = test(test_loader)
