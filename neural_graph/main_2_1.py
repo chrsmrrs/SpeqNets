b8b42ebfb17030a651ab7e1de07b6e7bc5a1b645
Christopher Morris <christopher.morris@udo.edu>
2021-11-23 21:02:35 -0500

    PPI

--- neural_graph/main_2_1.py
+++ neural_graph/main_2_1.py
@@ -60,10 +60,10 @@ class TUD_2_1(InMemoryDataset):
 
             print(i)
             x = data.x.cpu().detach().numpy()
-            x = x.argmax()
+            x = x.argmax(axis=-1)
 
             edge_attr = data.edge_attr.cpu().detach().numpy()
-            edge_attr = edge_attr.argmax()
+            edge_attr = edge_attr.argmax(axis=-1)
 
             print(x)
             print(edge_attr)
