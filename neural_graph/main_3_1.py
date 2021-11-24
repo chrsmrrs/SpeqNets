6c8b0283a560974fad9b2052aac4627fbdcdee0e
Christopher Morris <christopher.morris@udo.edu>
2021-11-23 21:54:12 -0500

    PPI

--- neural_graph/main_3_1.py
+++ neural_graph/main_3_1.py
@@ -84,7 +84,7 @@ class TUD_3_1(InMemoryDataset):
             data_new.edge_index_2 = torch.tensor(matrices[1]).t().contiguous().to(torch.long)
             data_new.edge_index_3 = torch.tensor(matrices[2]).t().contiguous().to(torch.long)
 
-            one_hot = np.eye(600)[labels]
+            one_hot = np.eye(700)[labels]
             data.x = torch.from_numpy(one_hot).to(torch.float)
 
             data_new.y = data.y
