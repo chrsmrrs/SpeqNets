b87a32551175f2ea6caf94a0f4e3150eec71efd0
Christopher Morris <christopher.morris@udo.edu>
2021-11-25 22:56:19 -0500

    PPI

--- neural_graph/main_3_1.py
+++ neural_graph/main_3_1.py
@@ -252,7 +252,7 @@ for _ in range(5):
     val_dataset = dataset[1600:1800]
     test_dataset = dataset[1800:]
 
-    batch_size = 25
+    batch_size = 5
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
