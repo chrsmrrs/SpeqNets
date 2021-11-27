2ef1c3ccb30371eba04f44836bfd40ea92e73387
Christopher Morris <christopher.morris@udo.edu>
2021-11-26 21:37:38 -0500

    PPI

--- neural_graph/main_1.py
+++ neural_graph/main_1.py
@@ -136,7 +136,7 @@ for _ in range(5):
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-    model = NetGIN(64).to(device)
+    model = NetGINE(64).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
                                                            factor=0.5, patience=5,
