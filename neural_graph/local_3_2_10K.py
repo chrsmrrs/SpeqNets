90989eabb487c10378a32a58e13734542380820b
Christopher Morris <christopher.morris@udo.edu>
2021-11-28 16:24:53 -0500

    PPI

--- neural_graph/local_3_2_10K.py
+++ neural_graph/local_3_2_10K.py
@@ -358,7 +358,7 @@ train_dataset = dataset[0:8000].shuffle()
 val_dataset = dataset[8000:9000].shuffle()
 test_dataset = dataset[9000:10000].shuffle()
 
-batch_size = 5
+batch_size = 1
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
