3581d29a9d2de5847e11b75d3e4117fa0b1df1b5
Christopher Morris <christopher.morris@udo.edu>
2021-11-28 21:08:09 -0500

    alc

--- neural_graph/local_3_2_10K.py
+++ neural_graph/local_3_2_10K.py
@@ -238,8 +238,6 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 path = osp.join(osp.dirname(osp.realpath(__file__)), '.', 'data', 'al10k')
 dataset = Alchemy_1(path, transform=MyTransform())
 
-print(len(dataset))
-exit()
 
 mean = dataset.data.y.mean(dim=0, keepdim=True)
 std = dataset.data.y.std(dim=0, keepdim=True)
