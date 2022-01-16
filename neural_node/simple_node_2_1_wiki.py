fb8c84c54356b2c1e0ca150bb96f101a1141e6cf
Christopher Morris <christopher.morris@udo.edu>
2022-01-15 21:24:43 -0500

    r

--- neural_node/simple_node_2_1_wiki.py
+++ neural_node/simple_node_2_1_wiki.py
@@ -204,7 +204,7 @@ def test(i):
 
 
 acc_all = []
-for i in range(5):
+for i in range(1):
     acc_total = 0
     for i in range(10):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
