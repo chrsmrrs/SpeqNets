580ffc055087dfb8e83c4d7c67d3b78ecbf85db6
Christopher Morris <christopher.morris@udo.edu>
2022-01-17 10:38:47 -0500

    r

--- neural_node/gnn_1_wiki.py
+++ neural_node/gnn_1_wiki.py
@@ -9,7 +9,7 @@ from torch_geometric.nn import GCNConv, ChebConv  # noqa
 
 dataset = 'chameleon'
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
-dataset = WikipediaNetwork(dataset, path)
+dataset = WikipediaNetwork(path, dataset)
 
 
 data = dataset[0]
