785f79b6fe14f2b33186fc6c5db23e02cc0e21bb
Christopher Morris <christopher.morris@udo.edu>
2022-01-17 18:57:29 -0500

    ghjhj

--- neural_node/simple_node_3_1.py
+++ neural_node/simple_node_3_1.py
@@ -173,7 +173,7 @@ class PPI_2_1(InMemoryDataset):
 
                     print(one_hot.shape, tmp.shape)
 
-                    type[t_v] = np.concatenate([tmp,at])
+                    type[t_v] = np.hstack([tmp,at])
 
                     print(type[t_v].shape)
                     exit()
