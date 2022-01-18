c87532b6bf2392a673885bda5e66ccf0def45513
Christopher Morris <christopher.morris@udo.edu>
2022-01-17 19:07:28 -0500

    ghjhj

--- neural_node/simple_node_3_1.py
+++ neural_node/simple_node_3_1.py
@@ -167,7 +167,7 @@ class PPI_2_1(InMemoryDataset):
 
                     tmp = np.concatenate([node_features[i] for i in t], axis=-1)
 
-                    one_hot = np.zeros((5,))
+                    one_hot = np.zeros((10,))
 
                     one_hot[int(at)] = 1
 
