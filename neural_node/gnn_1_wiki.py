3a6eec2abb249e6c74832d9e3b84eb2c1d6859b9
Christopher Morris <christopher.morris@udo.edu>
2022-01-18 20:38:35 -0500

    ghjhj

--- neural_node/gnn_1_wiki.py
+++ neural_node/gnn_1_wiki.py
@@ -13,18 +13,20 @@ path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', "ff")
 dataset = Twitch(path, name="PT")
 data = dataset[0]
 
+l = len(data.x)
+l = list(range(l))
+train, test = train_test_split(l, test_size=0.1)
+train, val = train_test_split(train, test_size=0.1)
 
-print(len(data.x))
-exit()
-train_index, val_index = train_test_split(list(), test_size=0.1)
+train_mask = [True if i in train else False for i in l]
+val_mask = [True if i in val else False for i in l]
+test_mask = [True if i in test else False for i in l]
 
 class Net(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = GCNConv(dataset.num_features, 16, cached=True)
         self.conv2 = GCNConv(16, dataset.num_classes, cached=True)
-
-
 
     def forward(self):
         x, edge_index, edge_weight = data.x, data.edge_index, data.edge_attr
@@ -34,46 +36,42 @@ class Net(torch.nn.Module):
 
 
 
-def train(i):
+def train():
     model.train()
     optimizer.zero_grad()
 
-    F.nll_loss(model()[data.train_mask[:,i]], data.y[data.train_mask[:,i]]).backward()
+    F.nll_loss(model()[train_mask], data.y[test_mask]).backward()
     optimizer.step()
 
 
 @torch.no_grad()
-def test(i):
+def test():
     model.eval()
     logits, accs = model(), []
-    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
+    for mask in [train_mask, val_mask, test_mask]:
 
-        pred = logits[mask[:,i]].max(1)[1]
-        acc = pred.eq(data.y[mask[:,i]]).sum().item() / mask[:,i].sum().item()
+        pred = logits[mask].max(1)[1]
+        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
         accs.append(acc)
     return accs
 
 acc_all = []
 for i in range(1):
     acc_total = 0
-    for i in range(10):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        model, data = Net().to(device), data.to(device)
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-3)
 
-        best_val_acc = test_acc = 0
-        for epoch in range(1, 201):
-            train(i)
-            train_acc, val_acc, tmp_test_acc = test(i)
-            if val_acc > best_val_acc:
-                best_val_acc = val_acc
-                test_acc = tmp_test_acc
-            print(i, f'Epoch: {epoch:03d}, Train: {train_acc:.4f}, '
-                     f'Val: {best_val_acc:.4f}, Test: {test_acc:.4f}')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model, data = Net().to(device), data.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-3)
 
-        acc_total += test_acc
+    best_val_acc = test_acc = 0
+    for epoch in range(1, 201):
+        train()
+        train_acc, val_acc, tmp_test_acc = test()
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            test_acc = tmp_test_acc
+        print(i, f'Epoch: {epoch:03d}, Train: {train_acc:.4f}, '
+                 f'Val: {best_val_acc:.4f}, Test: {test_acc:.4f}')
 
-    acc_all.append(acc_total/10)
 
-print(np.array(acc_all).mean(), np.array(acc_all).std())
-print(dataset.num_classes)
+print(test_acc)
