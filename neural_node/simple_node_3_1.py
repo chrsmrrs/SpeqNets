1b9f3c5b4ad6cc6bef421ff3b8c3f9ad518ca948
Christopher Morris <christopher.morris@udo.edu>
2022-01-20 15:27:51 -0500

    ghjhj

--- neural_node/simple_node_3_1.py
+++ neural_node/simple_node_3_1.py
@@ -39,18 +39,18 @@ class PPI_2_1(InMemoryDataset):
 
     @property
     def raw_file_names(self):
-        return "tetxfeetffttas"
+        return "tetxfreetffttas"
 
     @property
     def processed_file_names(self):
-        return "PPtI_2_t1eteetffffdgs"
+        return "PPtI_2_t1eteretffffdgs"
 
     def download(self):
         pass
 
     def process(self):
 
-        dataset = 'cornell'
+        dataset = 'texas'
         path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
         dataset = WebKB(path, dataset)
         data = dataset[0]
