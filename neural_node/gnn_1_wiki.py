61a3d869503e31853c7aeed053ea0d8fca9e0dca
Christopher Morris <christopher.morris@udo.edu>
2022-01-21 13:39:11 -0500

    ghjhj

--- neural_node/gnn_1_wiki.py
+++ neural_node/gnn_1_wiki.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', "feef")
-dataset = Actor(path)
+dataset = Twitch(path, name="DE")
 data = dataset[0]
 
 
