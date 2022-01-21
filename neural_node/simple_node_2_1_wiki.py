63b338115d4547111b9359fc312849533f3cae39
Christopher Morris <christopher.morris@udo.edu>
2022-01-21 14:18:10 -0500

    ghjhj

--- neural_node/simple_node_2_1_wiki.py
+++ neural_node/simple_node_2_1_wiki.py
@@ -194,8 +194,8 @@ class Net(torch.nn.Module):
 
         index_1 = index_1.to(torch.int64)
         index_2 = index_2.to(torch.int64)
-        x_1 = scatter(x, index_1, dim=0, reduce="add")
-        x_2 = scatter(x, index_2, dim=0, reduce="add")
+        x_1 = scatter(x, index_1, dim=0, reduce="mean")
+        x_2 = scatter(x, index_2, dim=0, reduce="mean")
         x = self.mlp(torch.cat([x_1, x_2], dim=1))
 
         return F.log_softmax(x, dim=1)
