d9742a16324a995d4ed299bc6cd26cc2e9782dbd
Christopher Morris <christopher.morris@udo.edu>
2022-01-21 13:09:42 -0500

    ghjhj

--- neural_node/simple_node_2_1_wiki.py
+++ neural_node/simple_node_2_1_wiki.py
@@ -150,7 +150,7 @@ class Net(torch.nn.Module):
         super(Net, self).__init__()
         dim = 256
 
-        self.mlp_init = Sequential(Linear(602, dim), ReLU(), Linear(dim, dim))
+        self.mlp_init = Sequential(Linear(1002, dim), ReLU(), Linear(dim, dim))
 
         self.conv_1_1 = GCNConv(dim, dim)
         self.conv_1_2 = GCNConv(dim, dim)
