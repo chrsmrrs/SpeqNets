2991a494954df24e71de4001946045f0c4f30b53
Christopher Morris <christopher.morris@udo.edu>
2022-01-22 18:58:24 -0500

    ghjhj

--- neural_graph/main_3_2_QM9_split.py
+++ neural_graph/main_3_2_QM9_split.py
@@ -40,7 +40,7 @@ class QM9(InMemoryDataset):
     def process(self):
         data_list = []
         targets = dp.get_dataset("QM9", multigregression=True).tolist()
-        attributes = pre.get_all_attributes_3_2("QM9")
+        attributes = pre.get_all_attributes_3_2("QM9", list(range(50000)))
 
         node_labels = pre.get_all_node_labels_3_2("QM9", False, False)
         matrices = pre.get_all_matrices_3_2("QM9", list(range(50000)))
